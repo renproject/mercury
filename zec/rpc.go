67c193666cad58e2992759bc14614efc97f5544a
Susruth  Nadimpalli <susruth@republicprotocol.com>
2019-03-27 14:08:52 +1100

    zec: add debug logs

--- zec/rpc.go
+++ zec/rpc.go
@@ -163,7 +163,7 @@ func (client *rpcClient) sendRequest(data []byte, response interface{}) error {
 	if resp.StatusCode != http.StatusOK {
 		return errors.New(string(msg))
 	}
-	fmt.Println(msg)
+	fmt.Println(string(msg))
 
 	result := Response{}
 	if err := json.Unmarshal(msg, &result); err != nil {
