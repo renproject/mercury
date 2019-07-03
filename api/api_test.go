ec5b6f1a29105e427a010eb405aa3682c1289c0e
Jaz Gulati <jaskaran.g15@gmail.com>
2019-07-03 14:01:53 +1000

    api_test: initialise api test

---
+++ api/api_test.go
@@ -0,0 +1,14 @@
+package api_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("APIs", func() {
+	Context("", func() {
+		It("", func() {
+			Expect(1).To(Equal(1))
+		})
+	})
+})
