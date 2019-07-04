11a12cc837f7af497bab9aed41d986468fd25a64
Vince Au <vinceau09@gmail.com>
2019-07-04 12:57:15 +1000

    Rename fee to gas

--- sdk/account/btcaccount/gas_test.go
+++ sdk/account/btcaccount/gas_test.go
@@ -11,24 +11,24 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-var _ = Describe("bitcoin tx fee", func() {
-	Context("when getting tx fee of different speed tier", func() {
+var _ = Describe("bitcoin tx gas", func() {
+	Context("when getting tx gas of different speed tier", func() {
 		It("should return the live data", func() {
 			logger := logrus.StandardLogger()
-			fee := NewBitcoinGas(logger, 5*time.Second)
+			gas := NewBitcoinGas(logger, 5*time.Second)
 
 			ctx := context.Background()
-			fastGas := fee.GasRequired(ctx, Fast)
+			fastGas := gas.GasRequired(ctx, Fast)
 			Expect(fastGas).Should(BeNumerically(">", 1))
-			logger.Infof("fast fee = %v sat/byte", fastGas)
+			logger.Infof("fast gas = %v sat/byte", fastGas)
 
-			standardGas := fee.GasRequired(ctx, Standard)
+			standardGas := gas.GasRequired(ctx, Standard)
 			Expect(standardGas).Should(BeNumerically(">", 1))
-			logger.Infof("standard fee = %v sat/byte", standardGas)
+			logger.Infof("standard gas = %v sat/byte", standardGas)
 
-			slowGas := fee.GasRequired(ctx, Slow)
+			slowGas := gas.GasRequired(ctx, Slow)
 			Expect(slowGas).Should(BeNumerically(">", 1))
-			logger.Infof("slow fee = %v sat/byte", slowGas)
+			logger.Infof("slow gas = %v sat/byte", slowGas)
 
 			Expect(fastGas >= standardGas).Should(BeTrue())
 			Expect(standardGas >= slowGas).Should(BeTrue())
