91a580de4fb1e9a444f5f643473401f6d7acd829
Vince Au <vinceau09@gmail.com>
2019-07-05 16:10:17 +1000

    types/btctypes: migrate transaction related code into separate file

--- types/btctypes/btctypes.go
+++ types/btctypes/btctypes.go
@@ -1,16 +1,12 @@
 package btctypes
 
 import (
-	"bytes"
 	"crypto/ecdsa"
 	"crypto/rand"
-	"fmt"
 	"strings"
 
 	"github.com/btcsuite/btcd/btcec"
 	"github.com/btcsuite/btcd/chaincfg"
-	"github.com/btcsuite/btcd/txscript"
-	"github.com/btcsuite/btcd/wire"
 	"github.com/btcsuite/btcutil"
 	"github.com/ethereum/go-ethereum/crypto/secp256k1"
 	"github.com/renproject/mercury/types"
@@ -123,127 +119,11 @@ func (utxos *UTXOs) Sum() Amount {
 	return total
 }
 
-type SignatureHash []byte
-
-type SerializedPubKey []byte
-
-type Signature btcec.Signature
-
-type Tx struct {
-	network   Network
-	tx        *wire.MsgTx
-	sigHashes []SignatureHash
-	signed    bool
-}
-
-func NewUnsignedTx(network Network, tx *wire.MsgTx) Tx {
-	return Tx{
-		network:   network,
-		tx:        tx,
-		sigHashes: []SignatureHash{},
-		signed:    false,
-	}
-}
-
-func (tx *Tx) IsSigned() bool {
-	return tx.signed
-}
-
-func (tx *Tx) Sign(key *ecdsa.PrivateKey) (err error) {
-	subScripts := tx.SignatureHashes()
-	sigs := make([]*btcec.Signature, len(subScripts))
-	for i, subScript := range subScripts {
-		sigs[i], err = (*btcec.PrivateKey)(key).Sign(subScript)
-		if err != nil {
-			return err
-		}
-	}
-	serializedPK := SerializePublicKey(&key.PublicKey, tx.network)
-	return tx.InjectSignatures(sigs, serializedPK)
-}
-
-// InjectSignatures injects the signed signatureHashes into the Tx. You cannot use the USTX anymore.
-func (tx *Tx) InjectSignatures(sigs []*btcec.Signature, serializedPubKey SerializedPubKey) error {
-	// Pre-condition checks
-	if tx.IsSigned() {
-		panic("pre-condition violation: cannot inject signatures into signed transaction")
-	}
-	if tx.tx == nil {
-		panic("pre-condition violation: cannot inject signatures into nil transaction")
-	}
-	if len(sigs) <= 0 {
-		panic("pre-condition violation: cannot inject empty signatures")
-	}
-	if len(sigs) != len(tx.tx.TxIn) {
-		panic(fmt.Errorf("pre-condition violation: expected signature len=%v to equal transaction input len=%v", len(sigs), len(tx.tx.TxIn)))
-	}
-	if len(serializedPubKey) <= 0 {
-		panic("pre-condition violation: cannot inject signatures with empty pubkey")
-	}
-
-	for i, sig := range sigs {
-		builder := txscript.NewScriptBuilder()
-		builder.AddData(append(sig.Serialize(), byte(txscript.SigHashAll)))
-		builder.AddData(serializedPubKey)
-		sigScript, err := builder.Script()
-		if err != nil {
-			return err
-		}
-		tx.tx.TxIn[i].SignatureScript = sigScript
-	}
-	tx.signed = true
-	return nil
-}
-
-func (tx *Tx) AppendSignatureHash(script []byte, mode txscript.SigHashType) error {
-	sigHash, err := txscript.CalcSignatureHash(script, mode, tx.tx, len(tx.sigHashes))
-	if err != nil {
-		return err
-	}
-	tx.sigHashes = append(tx.sigHashes, sigHash)
-	return nil
-}
-
-func (tx *Tx) ReplaceSignatureHash(script []byte, mode txscript.SigHashType, i int) (err error) {
-	if i < 0 || i >= len(tx.sigHashes) {
-		panic(fmt.Errorf("pre-condition violation: signature hash index=%v is out of range", i))
-	}
-	tx.sigHashes[i], err = txscript.CalcSignatureHash(script, mode, tx.tx, i)
-	return
-}
-
-// SignatureHashes returns a list of signature hashes need to be signed.
-func (tx *Tx) SignatureHashes() []SignatureHash {
-	return tx.sigHashes
-}
-
-// Serialize returns the serialized tx in bytes.
-func (tx *Tx) Serialize() []byte {
-	// Pre-condition checks
-	if tx.tx == nil {
-		panic("pre-condition violation: cannot serialize nil transaction")
-	}
-
-	buf := bytes.NewBuffer([]byte{})
-	if err := tx.tx.Serialize(buf); err != nil {
-		return nil
-	}
-	bufBytes := buf.Bytes()
-
-	// Post-condition checks
-	if len(bufBytes) <= 0 {
-		panic(fmt.Errorf("post-condition violation: serialized transaction len=%v is invalid", len(bufBytes)))
-	}
-	return bufBytes
-}
-
 type Recipient struct {
 	Address Address
 	Amount  Amount
 }
 
 type Recipients []Recipient
-
-type TxHash string
 
 type Confirmations int64
