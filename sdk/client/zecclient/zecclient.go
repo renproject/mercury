b9243e7af81ace6876dd2f74eb05915f95f85f6f
Jaz Gulati <jaskaran.g15@gmail.com>
2019-07-10 15:31:24 +1000

    sdk/client/zecclient, sdk/client/zecclient_test: add zec client

--- sdk/client/zecclient/zecclient.go
+++ sdk/client/zecclient/zecclient.go
@@ -1,1 +1,162 @@
 package zecclient
+
+import (
+	"fmt"
+
+	"github.com/btcsuite/btcd/chaincfg"
+	"github.com/btcsuite/btcd/chaincfg/chainhash"
+	"github.com/btcsuite/btcd/rpcclient"
+	"github.com/btcsuite/btcutil"
+	"github.com/renproject/mercury/types/zectypes"
+)
+
+const (
+	Dust               = zectypes.Amount(600)
+	MainnetMercuryURL  = "206.189.83.88:5000/zec/mainnet"
+	TestnetMercuryURL  = "206.189.83.88:5000/zec/testnet"
+	LocalnetMercuryURL = "0.0.0.0:5000/zec/testnet"
+)
+
+// TODO: This should use a common interface to Bitcoin (types should be generic)
+type Client interface {
+	Network() zectypes.Network
+	UTXOs(txHash zectypes.TxHash) (zectypes.UTXOs, error)
+	UTXOsFromAddress(address zectypes.Address) (zectypes.UTXOs, error)
+	Confirmations(txHash zectypes.TxHash) (zectypes.Confirmations, error)
+	BuildUnsignedTx(utxos zectypes.UTXOs, recipients zectypes.Recipients, refundTo zectypes.Address, gas zectypes.Amount) (zectypes.Tx, error)
+	SubmitSignedTx(stx zectypes.Tx) (zectypes.TxHash, error)
+}
+
+// Client is a client which is used to talking with certain ZCash network. It can interacting with the blockchain
+// through Mercury server.
+type client struct {
+	network zectypes.Network
+	client  *rpcclient.Client // FIXME: Remove this
+
+	config chaincfg.Params
+	url    string
+}
+
+// New returns a new Client of given ZCash network.
+func New(network zectypes.Network) (Client, error) {
+	config := &rpcclient.ConnConfig{
+		HTTPPostMode: true,
+		DisableTLS:   true,
+	}
+
+	switch network {
+	case zectypes.Mainnet:
+		config.Host = MainnetMercuryURL
+	case zectypes.Testnet:
+		config.Host = TestnetMercuryURL
+	case zectypes.Localnet:
+		config.Host = LocalnetMercuryURL
+	default:
+		panic("unknown zcash network")
+	}
+
+	c, err := rpcclient.New(config, nil)
+	if err != nil {
+		return &client{}, err
+	}
+
+	return &client{
+		network: network,
+		client:  c,
+		config:  *network.Params(),
+		url:     config.Host,
+	}, nil
+}
+
+func (c *client) Network() zectypes.Network {
+	return c.network
+}
+
+// UTXOs returns the UTXOs for the given transaction hash.
+func (c *client) UTXOs(txHash zectypes.TxHash) (zectypes.UTXOs, error) {
+	txHashBytes, err := chainhash.NewHashFromStr(string(txHash))
+	if err != nil {
+		return nil, fmt.Errorf("cannot parse hash: %v", err)
+	}
+	tx, err := c.client.GetTransaction(txHashBytes)
+	if err != nil {
+		return nil, fmt.Errorf("cannot get tx from hash %s: %v", txHash, err)
+	}
+
+	outputs := tx.Details
+	var utxos zectypes.UTXOs
+	for _, output := range outputs {
+		txOut, err := c.client.GetTxOut(txHashBytes, output.Vout, true)
+		if err != nil {
+			return nil, fmt.Errorf("cannot get tx output from zec client: %v", err)
+		}
+
+		// If the transaction output has been spent, continue.
+		if txOut == nil {
+			continue
+		}
+
+		amount, err := btcutil.NewAmount(txOut.Value)
+		if err != nil {
+			return nil, fmt.Errorf("cannot parse amount received from zec client: %v", err)
+		}
+		utxo := zectypes.UTXO{
+			TxHash:       zectypes.TxHash(tx.TxID),
+			Amount:       zectypes.Amount(amount),
+			ScriptPubKey: txOut.ScriptPubKey.Hex,
+			Vout:         output.Vout,
+		}
+		utxos = append(utxos, utxo)
+	}
+
+	return utxos, nil
+}
+
+// UTXOsFromAddress returns the UTXOs for a given address. Important: this function will not return any UTXOs for
+// addresses that have not been imported into the Bitcoin node.
+func (c *client) UTXOsFromAddress(address zectypes.Address) (zectypes.UTXOs, error) {
+	outputs, err := c.client.ListUnspentMinMaxAddresses(0, 999999, []btcutil.Address{address})
+	if err != nil {
+		return nil, fmt.Errorf("cannot retrieve utxos from zec client: %v", err)
+	}
+
+	utxos := make(zectypes.UTXOs, len(outputs))
+	for i, output := range outputs {
+		amount, err := btcutil.NewAmount(output.Amount)
+		if err != nil {
+			return nil, fmt.Errorf("cannot parse amount received from zec client: %v", err)
+		}
+		utxos[i] = zectypes.UTXO{
+			TxHash:       zectypes.TxHash(output.TxID),
+			Amount:       zectypes.Amount(amount),
+			ScriptPubKey: output.ScriptPubKey,
+			Vout:         output.Vout,
+		}
+	}
+
+	return utxos, nil
+}
+
+// Confirmations returns the number of confirmation blocks of the given txHash.
+func (c *client) Confirmations(txHash zectypes.TxHash) (zectypes.Confirmations, error) {
+	txHashBytes, err := chainhash.NewHashFromStr(string(txHash))
+	if err != nil {
+		return 0, fmt.Errorf("cannot parse hash: %v", err)
+	}
+	tx, err := c.client.GetTransaction(txHashBytes)
+	if err != nil {
+		return 0, fmt.Errorf("cannot get tx from hash %s: %v", txHash, err)
+	}
+
+	return zectypes.Confirmations(tx.Confirmations), nil
+}
+
+func (c *client) BuildUnsignedTx(utxos zectypes.UTXOs, recipients zectypes.Recipients, refundTo zectypes.Address, gas zectypes.Amount) (zectypes.Tx, error) {
+	panic("unimplemented")
+}
+
+// SubmitSignedTx submits the signed transactions
+// returns the transaction hash as in hex
+func (c *client) SubmitSignedTx(stx zectypes.Tx) (zectypes.TxHash, error) {
+	panic("unimplemented")
+}
--- sdk/client/zecclient/zecclient_test.go
+++ sdk/client/zecclient/zecclient_test.go
@@ -1,1 +1,120 @@
-package zecclient
+package zecclient_test
+
+import (
+	"log"
+	"os"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	. "github.com/renproject/mercury/sdk/client/zecclient"
+
+	"github.com/renproject/kv"
+	"github.com/renproject/mercury/api"
+	"github.com/renproject/mercury/cache"
+	"github.com/renproject/mercury/proxy"
+	"github.com/renproject/mercury/rpc/zecrpc"
+	"github.com/renproject/mercury/testutils"
+	"github.com/renproject/mercury/types/zectypes"
+	"github.com/sirupsen/logrus"
+)
+
+var _ = Describe("zec client", func() {
+	// loadTestAccounts loads a HD Extended key for this tests. Some addresses of certain path has been set up for this
+	// test. (i.e have known balance, utxos.)
+	loadTestAccounts := func(network zectypes.Network) testutils.HdKey {
+		wallet, err := testutils.LoadHdWalletFromEnv("ZEC_TEST_MNEMONIC", "ZEC_TEST_PASSPHRASE", network)
+		Expect(err).NotTo(HaveOccurred())
+		return wallet
+	}
+
+	BeforeSuite(func() {
+		logger := logrus.StandardLogger()
+		store := kv.NewJSON(kv.NewMemDB())
+		cache := cache.New(store, logger)
+
+		zecTestnetURL := os.Getenv("ZCASH_TESTNET_RPC_URL")
+		zecTestnetUser := os.Getenv("ZCASH_TESTNET_RPC_USERNAME")
+		zecTestnetPassword := os.Getenv("ZCASH_TESTNET_RPC_PASSWORD")
+		zecTestnetNodeClient, err := zecrpc.NewNodeClient(zecTestnetURL, zecTestnetUser, zecTestnetPassword)
+		Expect(err).ToNot(HaveOccurred())
+
+		zecTestnetProxy := proxy.NewProxy(zecTestnetNodeClient)
+		zecTestnetAPI := api.NewZecApi(zectypes.Testnet, zecTestnetProxy, cache, logger)
+		server := api.NewServer(logger, "5000", zecTestnetAPI)
+		go server.Run()
+	})
+
+	Context("when fetching UTXOs", func() {
+		It("should return a non-zero number of UTXOs for a transaction with unspent outputs", func() {
+			client, err := New(zectypes.Localnet)
+			Expect(err).NotTo(HaveOccurred())
+
+			utxos, err := client.UTXOs("e96953b5030f44686e71650d6cb71a83625059ad086f7fc7802775e22cef0f65")
+			Expect(err).NotTo(HaveOccurred())
+			log.Println(utxos)
+			Expect(len(utxos)).Should(BeNumerically(">", 0))
+		})
+
+		It("should return zero UTXOs for a transaction with spent outputs", func() {
+			client, err := New(zectypes.Localnet)
+			Expect(err).NotTo(HaveOccurred())
+
+			address, err := loadTestAccounts(zectypes.Localnet).ZECAddress(44, 1, 0, 0, 1)
+			Expect(err).NotTo(HaveOccurred())
+
+			log.Println(address)
+
+			utxos, err := client.UTXOs("d16e32d5e7b5442c8aaffe687ed0db7c2b4a7221a8607620902c06b214f8c4b1")
+			log.Println(utxos)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(len(utxos)).Should(Equal(0))
+		})
+
+		It("should return an error for an invalid transaction hash", func() {
+			client, err := New(zectypes.Localnet)
+			Expect(err).NotTo(HaveOccurred())
+
+			utxos, err := client.UTXOs("4b9e0e80d4bb9380e97aaa05fa872df57e65d34373491653934d32cc992211b1")
+			Expect(err).To(HaveOccurred())
+			Expect(len(utxos)).Should(Equal(0))
+		})
+
+		It("should return a non-zero number of UTXOs for a funded address that has been imported", func() {
+			client, err := New(zectypes.Localnet)
+			Expect(err).NotTo(HaveOccurred())
+			address, err := loadTestAccounts(zectypes.Localnet).ZECAddress(44, 1, 0, 0, 1)
+			Expect(err).NotTo(HaveOccurred())
+
+			utxos, err := client.UTXOsFromAddress(address)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(len(utxos)).Should(BeNumerically(">", 0))
+		})
+
+		It("should return zero UTXOs for a randomly generated address", func() {
+			client, err := New(zectypes.Localnet)
+			Expect(err).NotTo(HaveOccurred())
+			address, err := testutils.RandomZECAddress(zectypes.Localnet)
+			Expect(err).NotTo(HaveOccurred())
+
+			utxos, err := client.UTXOsFromAddress(address)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(len(utxos)).Should(Equal(0))
+		})
+	})
+
+	Context("when building a utx", func() {
+		PIt("should have the correct inputs", func() {
+			// TODO: write the test
+		})
+
+		PIt("should have the correct outputs", func() {
+			// TODO: write the test
+		})
+	})
+
+	Context("when submitting stx to bitcoin", func() {
+		PIt("should be able to submit a stx", func() {
+			// TODO: write the test
+		})
+	})
+})
