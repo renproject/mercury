d0d21fb515907fa212daf8747012c337b4d2c4f8
susruth <susruth@republicprotocol.com>
2019-07-11 14:36:58 +1000

    sdk/client/zecclient: add BuildUnsignedTx

--- sdk/client/zecclient/zecclient.go
+++ sdk/client/zecclient/zecclient.go
@@ -3,18 +3,22 @@ package zecclient
 import (
 	"encoding/hex"
 	"fmt"
-	"log"
 
 	"github.com/btcsuite/btcd/btcjson"
 	"github.com/btcsuite/btcd/chaincfg"
 	"github.com/btcsuite/btcd/chaincfg/chainhash"
 	"github.com/btcsuite/btcd/rpcclient"
 	"github.com/btcsuite/btcd/txscript"
+	"github.com/btcsuite/btcd/wire"
 	"github.com/btcsuite/btcutil"
+	"github.com/iqoption/zecutil"
 	"github.com/renproject/mercury/types/zectypes"
 )
 
 const (
+	// TODO: Understand what this is, and set it to a reasonable value.
+	ExpiryHeight       = uint32(10000000)
+	Version            = 4
 	Dust               = zectypes.Amount(600)
 	MainnetMercuryURL  = "206.189.83.88:5000/zec/mainnet"
 	TestnetMercuryURL  = "206.189.83.88:5000/zec/testnet"
@@ -158,10 +162,90 @@ func (c *client) Confirmations(txHash zectypes.TxHash) (zectypes.Confirmations, 
 }
 
 func (c *client) BuildUnsignedTx(utxos zectypes.UTXOs, recipients zectypes.Recipients, refundTo zectypes.Address, gas zectypes.Amount) (zectypes.Tx, error) {
-	panic("unimplemented")
+	// Pre-condition checks
+	if gas < Dust {
+		return zectypes.Tx{}, fmt.Errorf("pre-condition violation: gas = %v is too low", gas)
+	}
+
+	inputs := make([]btcjson.TransactionInput, len(utxos))
+	for i, utxo := range utxos {
+		inputs[i] = btcjson.TransactionInput{
+			Txid: string(utxo.TxHash),
+			Vout: utxo.Vout,
+		}
+	}
+
+	amountFromUTXOs := utxos.Sum()
+	if amountFromUTXOs < Dust {
+		return zectypes.Tx{}, fmt.Errorf("pre-condition violation: amount=%v from utxos is less than dust=%v", amountFromUTXOs, Dust)
+	}
+
+	// Add an output for each recipient and sum the total amount that is being
+	// transferred to recipients
+	amountToRecipients := zectypes.Amount(0)
+	outputs := make(map[btcutil.Address]btcutil.Amount, len(recipients))
+	for _, recipient := range recipients {
+		amountToRecipients += recipient.Amount
+		outputs[recipient.Address] = btcutil.Amount(recipient.Amount)
+	}
+
+	// Check that we are not transferring more to recipients than available in
+	// the UTXOs (accounting for gas)
+	amountToRefund := amountFromUTXOs - amountToRecipients - gas
+	if amountToRefund < 0 {
+		return zectypes.Tx{}, fmt.Errorf("insufficient balance: expected %v, got %v", amountToRecipients+gas, amountFromUTXOs)
+	}
+
+	// Add an output to refund the difference between what we are transferring
+	// to recipients and what we are spending from the UTXOs (accounting for
+	// gas)
+	outputs[refundTo] += btcutil.Amount(amountToRefund)
+
+	wireTx, err := createRawTransaction(inputs, outputs)
+	if err != nil {
+		return zectypes.Tx{}, fmt.Errorf("cannot construct raw transaction: %v", err)
+	}
+
+	// Get the signature hashes we need to sign
+	unsignedTx := zectypes.NewUnsignedTx(c.network, utxos, wireTx)
+	for _, utxo := range utxos {
+		scriptPubKey, err := hex.DecodeString(utxo.ScriptPubKey)
+		if err != nil {
+			return zectypes.Tx{}, err
+		}
+		if err := unsignedTx.AppendSignatureHash(scriptPubKey, txscript.SigHashAll, int64(utxo.Amount)); err != nil {
+			return zectypes.Tx{}, err
+		}
+	}
+	return unsignedTx, nil
 }
 
 // SubmitSignedTx submits the signed transaction and returns the transaction hash in hex.
 func (c *client) SubmitSignedTx(stx zectypes.Tx) (zectypes.TxHash, error) {
 	panic("unimplemented")
+}
+
+func createRawTransaction(inputs []btcjson.TransactionInput, amounts map[btcutil.Address]btcutil.Amount) (*zecutil.MsgTx, error) {
+	wireTx := zecutil.MsgTx{
+		MsgTx:        wire.NewMsgTx(Version),
+		ExpiryHeight: ExpiryHeight,
+	}
+
+	for _, input := range inputs {
+		hash, err := chainhash.NewHashFromStr(input.Txid)
+		if err != nil {
+			return nil, err
+		}
+		wireTx.AddTxIn(wire.NewTxIn(wire.NewOutPoint(hash, input.Vout), nil, nil))
+	}
+
+	for address, amount := range amounts {
+		script, err := zecutil.PayToAddrScript(address)
+		if err != nil {
+			return nil, err
+		}
+		wireTx.AddTxOut(wire.NewTxOut(int64(amount.ToUnit(btcutil.AmountSatoshi)), script))
+	}
+
+	return &wireTx, nil
 }
