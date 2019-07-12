3555b27431853dd8bcda942b0f164bbb984f3285
susruth <susruth@republicprotocol.com>
2019-07-12 15:55:52 +1000

    types/btctypes/btcaddress: simplified address creation

--- types/btctypes/btcaddress/address.go
+++ types/btctypes/btcaddress/address.go
@@ -2,9 +2,11 @@ package btcaddress
 
 import (
 	"crypto/ecdsa"
+	"fmt"
 
 	"github.com/btcsuite/btcd/btcec"
 	"github.com/btcsuite/btcutil"
+	"github.com/iqoption/zecutil"
 	"github.com/renproject/mercury/types"
 	"github.com/renproject/mercury/types/btctypes"
 )
@@ -30,5 +32,57 @@ func SerializePublicKey(pubkey *ecdsa.PublicKey, network btctypes.Network) []byt
 		return (*btcec.PublicKey)(pubkey).SerializeUncompressed()
 	default:
 		panic(types.ErrUnknownNetwork)
+	}
+}
+
+// AddressFromBase58 decodes the base58 encoded address to an `Address`.
+func AddressFromBase58(addr string, chain btctypes.Chain, network btctypes.Network) (Address, error) {
+	switch chain {
+	case btctypes.Bitcoin:
+		return btcutil.DecodeAddress(addr, network.Params())
+	case btctypes.ZCash:
+		return zecutil.DecodeAddress(addr, network.Params().Name)
+	default:
+		return nil, fmt.Errorf("unsupported blockchain: %d", chain)
+	}
+}
+
+// AddressFromPubKey gets the `Address` from a public key.
+func AddressFromPubKey(pubkey *ecdsa.PublicKey, chain btctypes.Chain, network btctypes.Network) (Address, error) {
+	switch chain {
+	case btctypes.Bitcoin:
+		addr, err := btcutil.NewAddressPubKey(SerializePublicKey(pubkey, network), network.Params())
+		if err != nil {
+			return nil, fmt.Errorf("cannot decode address from public key: %v", err)
+		}
+		return btcutil.DecodeAddress(addr.EncodeAddress(), network.Params())
+	case btctypes.ZCash:
+		addr, err := zecAddressFromHash160(btcutil.Hash160(SerializePublicKey(pubkey, network)), network.Params(), false)
+		if err != nil {
+			return nil, fmt.Errorf("cannot decode address from public key: %v", err)
+		}
+		return zecutil.DecodeAddress(addr.EncodeAddress(), network.Params().Name)
+	default:
+		return nil, fmt.Errorf("unsupported blockchain: %d", chain)
+	}
+}
+
+// AddressFromScript gets the `Address` from a script.
+func AddressFromScript(script []byte, chain btctypes.Chain, network btctypes.Network) (Address, error) {
+	switch chain {
+	case btctypes.Bitcoin:
+		addr, err := btcutil.NewAddressScriptHash(script, network.Params())
+		if err != nil {
+			return nil, fmt.Errorf("cannot decode address from public key: %v", err)
+		}
+		return btcutil.DecodeAddress(addr.EncodeAddress(), network.Params())
+	case btctypes.ZCash:
+		addr, err := zecAddressFromHash160(btcutil.Hash160(script), network.Params(), true)
+		if err != nil {
+			return nil, fmt.Errorf("cannot decode address from public key: %v", err)
+		}
+		return zecutil.DecodeAddress(addr.EncodeAddress(), network.Params().Name)
+	default:
+		return nil, fmt.Errorf("unsupported blockchain: %d", chain)
 	}
 }
--- types/btctypes/btcaddress/zec.go
+++ types/btctypes/btcaddress/zec.go
@@ -1,33 +1,15 @@
 package btcaddress
 
 import (
-	"crypto/ecdsa"
 	"crypto/sha256"
-	"fmt"
 
 	"github.com/btcsuite/btcd/chaincfg"
 	"github.com/btcsuite/btcutil"
 	"github.com/btcsuite/btcutil/base58"
 	"github.com/iqoption/zecutil"
-	"github.com/renproject/mercury/types/btctypes"
 )
 
-// ZecAddressFromBase58 decodes the base58 encoding ZCash address to an `Address`.
-func ZecAddressFromBase58(addr string, network btctypes.Network) (Address, error) {
-	return zecutil.DecodeAddress(addr, network.Params().Name)
-}
-
-// ZecAddressFromPubKey gets the `Address` from a public key.
-func ZecAddressFromPubKey(pubkey *ecdsa.PublicKey, network btctypes.Network) (Address, error) {
-	addr, err := addressFromHash160(btcutil.Hash160(SerializePublicKey(pubkey, network)), network.Params(), false)
-	if err != nil {
-		return nil, fmt.Errorf("cannot decode address from public key: %v", err)
-	}
-
-	return zecutil.DecodeAddress(addr.EncodeAddress(), network.Params().Name)
-}
-
-func addressFromHash160(hash []byte, params *chaincfg.Params, isScript bool) (btcutil.Address, error) {
+func zecAddressFromHash160(hash []byte, params *chaincfg.Params, isScript bool) (btcutil.Address, error) {
 	prefixes := map[string]map[string][]byte{
 		"mainnet": map[string][]byte{
 			"pubkey": []byte{0x1C, 0xB8},
