7bc03e011c4fecb3b6adadb0a565008fbf15b6c0
Vince Au <vinceau09@gmail.com>
2019-07-16 10:15:27 +1000

    sdk/client/btcclient: print the correct sat/byte amount

--- sdk/client/btcclient/gas_test.go
+++ sdk/client/btcclient/gas_test.go
@@ -20,17 +20,17 @@ var _ = Describe("bitcoin tx gas", func() {
 			gas := NewBtcGasStation(logger, 5*time.Second)
 
 			ctx := context.Background()
-			fastGas, err := gas.GasRequired(ctx, types.Fast, 100)
+			fastGas, err := gas.GasRequired(ctx, types.Fast, 1)
 			Expect(err).Should(BeNil())
 			Expect(fastGas).Should(BeNumerically(">", 1))
 			logger.Infof("fast gas = %v sat/byte", fastGas)
 
-			standardGas, err := gas.GasRequired(ctx, types.Standard, 100)
+			standardGas, err := gas.GasRequired(ctx, types.Standard, 1)
 			Expect(err).Should(BeNil())
 			Expect(standardGas).Should(BeNumerically(">", 1))
 			logger.Infof("standard gas = %v sat/byte", standardGas)
 
-			slowGas, err := gas.GasRequired(ctx, types.Slow, 100)
+			slowGas, err := gas.GasRequired(ctx, types.Slow, 1)
 			Expect(err).Should(BeNil())
 			Expect(slowGas).Should(BeNumerically(">", 1))
 			logger.Infof("slow gas = %v sat/byte", slowGas)
