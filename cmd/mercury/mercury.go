8e077ff40fb8723374fb81da66fbedc02a0e2c0d
Jaz Gulati <jaskaran.g15@gmail.com>
2019-07-23 15:47:32 +1000

    cmd/mercury: update clients

--- cmd/mercury/mercury.go
+++ cmd/mercury/mercury.go
@@ -33,51 +33,10 @@ func main() {
 		logger.AddHook(hook)
 	}
 
-	mainnetBIClient, err := btc.NewBI("mainnet")
-	if err != nil {
-		logger.Error(err)
-		return
-	}
-	testnetBIClient, err := btc.NewBI("testnet")
-	if err != nil {
-		logger.Error(err)
-		return
-	}
-
-	mainnetCSClient, err := btc.NewCS("mainnet")
-	if err != nil {
-		logger.Error(err)
-		return
-	}
-	testnetCSClient, err := btc.NewCS("testnet")
-	if err != nil {
-		logger.Error(err)
-		return
-	}
-
-	zecMainnetCSClient, err := zec.NewCS("mainnet")
-	if err != nil {
-		logger.Error(err)
-		return
-	}
-	// zecTestnetCSClient, err := zec.NewCS("testnet")
-	// if err != nil {
-	// 	logger.Error(err)
-	// 	return
-	// }
-
-	// mainnetFNClient := btc.NewFN("mainnet", os.Getenv("BITCOIN_MAINNET_RPC_URL"), os.Getenv("BITCOIN_MAINNET_RPC_USER"), os.Getenv("BITCOIN_MAINNET_RPC_PASSWORD"))
-	testnetFNClient := btc.NewFN("testnet", os.Getenv("BITCOIN_TESTNET_RPC_URL"), os.Getenv("TESTNET_RPC_USER"), os.Getenv("TESTNET_RPC_PASSWORD"))
-	omniTestnetFNClient := btc.NewFN("testnet", os.Getenv("OMNI_TESTNET_RPC_URL"), os.Getenv("TESTNET_RPC_USER"), os.Getenv("TESTNET_RPC_PASSWORD"))
-	omniMainnetFNClient := btc.NewFN("mainnet", os.Getenv("OMNI_MAINNET_RPC_URL"), os.Getenv("BITCOIN_MAINNET_RPC_USER"), os.Getenv("BITCOIN_MAINNET_RPC_PASSWORD"))
-
-	zecTestnetFNClient := zec.NewFN("testnet", os.Getenv("ZCASH_TESTNET_RPC_URL"), os.Getenv("TESTNET_RPC_USER"), os.Getenv("TESTNET_RPC_PASSWORD"))
-	// zecMainnetFNClient := zec.NewFN(os.Getenv("ZCASH_MAINNET_RPC_URL"), os.Getenv("ZCASH_MAINNET_RPC_USER"), os.Getenv("ZCASH_MAINNET_RPC_PASSWORD"), "mainnet")
-
-	btcMainnetPlugin := btc.New("btc", btc.NewMulti(mainnetBIClient, mainnetCSClient, omniMainnetFNClient), logger)
-	btcTestnetPlugin := btc.New("btc-testnet3", btc.NewMulti(testnetFNClient, testnetBIClient, testnetCSClient, omniTestnetFNClient), logger)
-	zecTestnetPlugin := zec.New("zec", zec.NewMulti(zecMainnetCSClient), logger)
-	zecMainnetPlugin := zec.New("zec-testnet", zecTestnetFNClient, logger)
+	testnetFNClient := btc.NewFN("testnet", os.Getenv("BITCOIN_TESTNET_RPC_URL"), os.Getenv("BITCOIN_TESTNET_RPC_USERNAME"), os.Getenv("BITCOIN_TESTNET_RPC_PASSWORD"))
+	zecTestnetFNClient := zec.NewFN("testnet", os.Getenv("ZCASH_TESTNET_RPC_URL"), os.Getenv("ZCASH_TESTNET_RPC_USERNAME"), os.Getenv("ZCASH_TESTNET_RPC_PASSWORD"))
+	btcTestnetPlugin := btc.New("btc-testnet3", testnetFNClient, logger)
+	zecTestnetPlugin := zec.New("zec-testnet", zecTestnetFNClient, logger)
 	apiKeys := map[string]string{
 		"":         os.Getenv("INFURA_KEY_DEFAULT"),
 		"swapperd": os.Getenv("INFURA_KEY_SWAPPERD"),
@@ -90,7 +49,5 @@ func main() {
 	kovanEthPlugin := eth.New("eth-kovan", privKey, apiKeys, logger)
 	ropstenEthPlugin := eth.New("eth-ropsten", privKey, apiKeys, logger)
 	mainnetEthPlugin := eth.New("eth", privKey, apiKeys, logger)
-	mercury.New(os.Getenv("PORT"), logger, btcMainnetPlugin, zecMainnetPlugin,
-		btcTestnetPlugin, zecTestnetPlugin, kovanEthPlugin, ropstenEthPlugin,
-		mainnetEthPlugin).Run()
+	mercury.New(os.Getenv("PORT"), logger, btcTestnetPlugin, zecTestnetPlugin, kovanEthPlugin, ropstenEthPlugin, mainnetEthPlugin).Run()
 }
