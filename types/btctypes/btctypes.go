1484fca69509b0107494ab68137315389874deb9
susruth <susruth@republicprotocol.com>
2019-07-23 16:17:43 +1000

    types/btctypes: moved amounts to a new file

--- types/btctypes/btctypes.go
+++ types/btctypes/btctypes.go
@@ -13,19 +13,6 @@ type Network interface {
 	Params() *chaincfg.Params
 }
 
-// Amount represents the value in the smallest possible unit for the respective blockchain.
-type Amount int64
-
-const (
-	SAT = Amount(1)
-	BTC = Amount(1e8 * SAT)
-)
-
-const (
-	ZAT = Amount(1)
-	ZEC = Amount(1e8 * ZAT)
-)
-
 type network uint8
 
 const (
