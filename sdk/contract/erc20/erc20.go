a49aa89bb6e33b74e2ed7d72f4caa08cc91d4c6b
Vince Au <vinceau09@gmail.com>
2019-07-30 12:29:15 +1000

    sdk/contract/erc20: ERC20 amounts should be big ints

--- sdk/contract/erc20/erc20.go
+++ sdk/contract/erc20/erc20.go
@@ -11,13 +11,13 @@ import (
 
 // ERC20 is a bindings object for Erc20
 type ERC20 interface {
-	Balance(ctx context.Context, address ethtypes.Address) (ethtypes.Amount, error)
-	TotalSupply(ctx context.Context) (ethtypes.Amount, error)
+	Balance(ctx context.Context, address ethtypes.Address) (*big.Int, error)
+	TotalSupply(ctx context.Context) (*big.Int, error)
 	Decimals(ctx context.Context) (uint8, error)
 
-	Transfer(ctx context.Context, signer, to ethtypes.Address, amount ethtypes.Amount) (ethtypes.Tx, error)
-	Approve(ctx context.Context, signer, to ethtypes.Address, amount ethtypes.Amount) (ethtypes.Tx, error)
-	TransferFrom(ctx context.Context, signer, from, to ethtypes.Address, amount ethtypes.Amount) (ethtypes.Tx, error)
+	Transfer(ctx context.Context, signer, to ethtypes.Address, amount *big.Int) (ethtypes.Tx, error)
+	Approve(ctx context.Context, signer, to ethtypes.Address, amount *big.Int) (ethtypes.Tx, error)
+	TransferFrom(ctx context.Context, signer, from, to ethtypes.Address, amount *big.Int) (ethtypes.Tx, error)
 
 	Watch(ctx context.Context, events chan<- ethtypes.Event, beginBlockNum *big.Int) error
 }
@@ -40,21 +40,21 @@ func New(client ethclient.Client, addr ethtypes.Address) (ERC20, error) {
 }
 
 // Balance returns the balance of an address
-func (e *erc20) Balance(ctx context.Context, address ethtypes.Address) (ethtypes.Amount, error) {
+func (e *erc20) Balance(ctx context.Context, address ethtypes.Address) (*big.Int, error) {
 	balance := new(*big.Int)
 	if err := e.contract.Call(ctx, ethtypes.Address{}, &balance, "balanceOf", address); err != nil {
-		return ethtypes.Amount{}, err
+		return nil, err
 	}
-	return ethtypes.WeiFromBig(*balance), nil
+	return *balance, nil
 }
 
 // TotalSupply returns the total supply of an ERC20
-func (e *erc20) TotalSupply(ctx context.Context) (amount ethtypes.Amount, err error) {
+func (e *erc20) TotalSupply(ctx context.Context) (amount *big.Int, err error) {
 	totalSupply := new(*big.Int)
 	if err := e.contract.Call(ctx, ethtypes.Address{}, &totalSupply, "totalSupply"); err != nil {
-		return ethtypes.Amount{}, err
+		return nil, err
 	}
-	return ethtypes.WeiFromBig(*totalSupply), nil
+	return *totalSupply, nil
 }
 
 // Decimals returns the total supply of an ERC20
@@ -64,17 +64,17 @@ func (e *erc20) Decimals(ctx context.Context) (decimals uint8, err error) {
 }
 
 // Transfer the given amount of ERC20 to the`to` address
-func (e *erc20) Transfer(ctx context.Context, signer, to ethtypes.Address, amount ethtypes.Amount) (ethtypes.Tx, error) {
+func (e *erc20) Transfer(ctx context.Context, signer, to ethtypes.Address, amount *big.Int) (ethtypes.Tx, error) {
 	return e.contract.BuildTx(ctx, signer, "transfer", nil, to, amount)
 }
 
 // Approve the given amount of ERC20 to the `to` address from the `from` address
-func (e *erc20) Approve(ctx context.Context, signer, to ethtypes.Address, amount ethtypes.Amount) (ethtypes.Tx, error) {
+func (e *erc20) Approve(ctx context.Context, signer, to ethtypes.Address, amount *big.Int) (ethtypes.Tx, error) {
 	return e.contract.BuildTx(ctx, signer, "approve", nil, to, amount)
 }
 
 // TransferFrom the given amount of ERC20 to the `to` address from the `from` address
-func (e *erc20) TransferFrom(ctx context.Context, signer, from, to ethtypes.Address, amount ethtypes.Amount) (ethtypes.Tx, error) {
+func (e *erc20) TransferFrom(ctx context.Context, signer, from, to ethtypes.Address, amount *big.Int) (ethtypes.Tx, error) {
 	return e.contract.BuildTx(ctx, signer, "transferFrom", nil, from, to, amount)
 }
 
