357eec5a0ee711bf155d1ffb3866d26753a474db
Vince Au <vinceau09@gmail.com>
2019-08-13 15:54:24 +1000

    stat: insert and get method call counts

--- stat/stat.go
+++ stat/stat.go
@@ -1,23 +1,41 @@
 package stat
 
+import (
+	"sync"
+	"time"
+)
+
 type Stat struct {
 	// requests is a
-	requests map[string][]int64
-	requestsMu
+	requestTimes map[string]map[int]int
+	requestsMu   *sync.Mutex
 }
 
 func NewStat() Stat {
-	requestTimes := make(map[string][]int64)
-	return Stat{requestTimes}
+	requestTimes := make(map[string]map[int]int)
+	return Stat{
+		requestTimes: requestTimes,
+		requestsMu:   &sync.Mutex{},
+	}
 }
 
 func (stat *Stat) Get() map[string]int {
 	numRequests := make(map[string]int)
-	for method, timestamps := range stat.requestTimes {
-
+	stat.requestsMu.Lock()
+	for method, hourTimestamps := range stat.requestTimes {
+		total := 0
+		for _, count := range hourTimestamps {
+			total += count
+		}
+		numRequests[method] = total
 	}
+	stat.requestsMu.Unlock()
+	return numRequests
 }
 
 func (stat *Stat) Insert(method string) {
-	stat.numRequests[method]++
+	t := time.Now()
+	stat.requestsMu.Lock()
+	stat.requestTimes[method][t.Hour()]++
+	stat.requestsMu.Unlock()
 }
