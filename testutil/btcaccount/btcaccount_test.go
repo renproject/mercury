7f6dd003bb1ada8a6d882b4ff0a178ba170bd9a7
susruth <susruth@renproject.io>
2019-09-01 22:32:47 +1000

    testutil/btcaccount: add ZCash test

--- testutil/btcaccount/btcaccount_test.go
+++ testutil/btcaccount/btcaccount_test.go
@@ -52,13 +52,41 @@ var _ = Describe("btc account", func() {
 	})
 
 	// FIXME: Do not run multiple tests with the same keypair.
-	FContext("when transferring funds", func() {
+	Context("when transferring funds", func() {
 		It("should be able to transfer funds to itself", func() {
 			// Get the account with actual balance
 			client := btcclient.NewClient(logger, btctypes.BtcLocalnet)
 			wallet, err := testutil.LoadHdWalletFromEnv("BTC_TEST_MNEMONIC", "BTC_TEST_PASSPHRASE", client.Network())
 			Expect(err).NotTo(HaveOccurred())
 			key, err := wallet.EcdsaKey(44, 1, 0, 0, 2)
+			Expect(err).NotTo(HaveOccurred())
+			account, err := NewAccount(client, key)
+			Expect(err).NotTo(HaveOccurred())
+			fmt.Println("from address: ", account.Address().EncodeAddress())
+
+			ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+			defer cancel()
+
+			utxos, err := account.UTXOs(ctx)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(len(utxos)).Should(BeNumerically(">", 0))
+
+			// Build the transaction
+			toAddress := account.Address()
+			amount := 50000 * btctypes.SAT
+			fmt.Println("to address: ", toAddress.EncodeAddress())
+
+			txHash, err := account.Transfer(ctx, toAddress, amount, types.Standard, true)
+			Expect(err).NotTo(HaveOccurred())
+			fmt.Println("txHash: ", txHash[:])
+		})
+
+		It("should be able to transfer funds to itself", func() {
+			// Get the account with actual balance
+			client := btcclient.NewClient(logger, btctypes.ZecLocalnet)
+			wallet, err := testutil.LoadHdWalletFromEnv("ZEC_TEST_MNEMONIC", "ZEC_TEST_PASSPHRASE", client.Network())
+			Expect(err).NotTo(HaveOccurred())
+			key, err := wallet.EcdsaKey(44, 1, 0, 0, 1)
 			Expect(err).NotTo(HaveOccurred())
 			account, err := NewAccount(client, key)
 			Expect(err).NotTo(HaveOccurred())
