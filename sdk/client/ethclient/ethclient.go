503596042611a2344dd116fbe300fa0fa07e1b0f
susruth <susruth@renproject.io>
2019-09-04 15:51:24 +1000

    sdk/client/ethclient: updated PublishSignedTx

--- sdk/client/ethclient/ethclient.go
+++ sdk/client/ethclient/ethclient.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	"github.com/ethereum/go-ethereum/common"
+	cTypes "github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/ethclient"
 	mclient "github.com/renproject/mercury/sdk/client"
 	"github.com/renproject/mercury/types"
@@ -19,12 +20,12 @@ type Client interface {
 	Balance(context.Context, ethtypes.Address) (ethtypes.Amount, error)
 	BlockNumber(context.Context) (*big.Int, error)
 	Contract(address ethtypes.Address, abi []byte) (ethtypes.Contract, error)
-	Confirmations(ctx context.Context, hash ethtypes.TxHash) (*big.Int, error)
+	Confirmations(ctx context.Context, hash types.TxHash) (*big.Int, error)
 	EthClient() *ethclient.Client
 	SuggestGasPrice(context.Context, types.TxSpeed) ethtypes.Amount
 	PendingNonceAt(context.Context, ethtypes.Address) (uint64, error)
 	BuildUnsignedTx(context.Context, uint64, ethtypes.Address, ethtypes.Amount, uint64, ethtypes.Amount, []byte) (ethtypes.Tx, error)
-	PublishSignedTx(context.Context, ethtypes.Tx) (ethtypes.TxHash, error)
+	PublishSignedTx(context.Context, ethtypes.Tx) (types.TxHash, error)
 	GasLimit(context.Context) (uint64, error)
 }
 
@@ -37,16 +38,7 @@ type client struct {
 
 // New returns a new Client of given ethereum network.
 func New(logger logrus.FieldLogger, network ethtypes.Network) (Client, error) {
-	var url string
-	switch network {
-	case ethtypes.Mainnet:
-		url = fmt.Sprintf("%s/eth/mainnet", mclient.MercuryURL)
-	case ethtypes.Kovan:
-		url = fmt.Sprintf("%s/eth/kovan", mclient.MercuryURL)
-	default:
-		return nil, types.ErrUnknownNetwork
-	}
-	return NewCustomClient(logger, url)
+	return NewCustomClient(logger, fmt.Sprintf("%s/%s/%s", mclient.MercuryURL, network.Chain(), network.String()))
 }
 
 // NewCustomClient returns an Client for a specific RPC url
@@ -104,26 +96,34 @@ func (c *client) PendingNonceAt(ctx context.Context, fromAddress ethtypes.Addres
 func (c *client) BuildUnsignedTx(ctx context.Context, nonce uint64, toAddress ethtypes.Address, value ethtypes.Amount, gasLimit uint64, gasPrice ethtypes.Amount, data []byte) (ethtypes.Tx, error) {
 	chainID, err := c.client.NetworkID(ctx)
 	if err != nil {
-		return ethtypes.Tx{}, fmt.Errorf("error building unsigned tx. failed to get chain id. err=%v", err)
+		return nil, fmt.Errorf("error building unsigned tx. failed to get chain id. err=%v", err)
 	}
 	return ethtypes.NewUnsignedTx(chainID, nonce, toAddress, value, gasLimit, gasPrice, data), nil
 }
 
 // PublishSTX publishes a signed transaction
-func (c *client) PublishSignedTx(ctx context.Context, tx ethtypes.Tx) (ethtypes.TxHash, error) {
+func (c *client) PublishSignedTx(ctx context.Context, tx ethtypes.Tx) (types.TxHash, error) {
 	// Pre-condition checks
 	if !tx.IsSigned() {
 		panic("pre-condition violation: cannot publish unsigned transaction")
 	}
-	return tx.Hash(), c.client.SendTransaction(ctx, tx.ToTransaction())
+	txBytes, err := tx.Serialize()
+	if err != nil {
+		return tx.Hash(), err
+	}
+	stx := new(cTypes.Transaction)
+	if err := stx.UnmarshalJSON(txBytes); err != nil {
+		return tx.Hash(), err
+	}
+	return tx.Hash(), c.client.SendTransaction(ctx, stx)
 }
 
-func (c *client) Confirmations(ctx context.Context, hash ethtypes.TxHash) (*big.Int, error) {
+func (c *client) Confirmations(ctx context.Context, hash types.TxHash) (*big.Int, error) {
 	currentBlockNumber, err := c.BlockNumber(ctx)
 	if err != nil {
 		return nil, fmt.Errorf("error fetching current block number: %v", err)
 	}
-	receipt, err := c.client.TransactionReceipt(ctx, common.Hash(hash))
+	receipt, err := c.client.TransactionReceipt(ctx, common.HexToHash(string(hash)))
 	if err != nil {
 		return nil, fmt.Errorf("error fetching tx hash=%v receipt: %v", hash, err)
 	}
