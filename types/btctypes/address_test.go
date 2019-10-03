0c0fa980079be8a31047f71f8ee7ec39c82b06ba
susruth <susruth@susruth.com>
2019-10-03 10:55:52 +1000

    types/btctypes: removed focus on a test

--- types/btctypes/address_test.go
+++ types/btctypes/address_test.go
@@ -8,7 +8,7 @@ import (
 
 var _ = Describe("btctypes", func() {
 	Context("when decoding testnet zcash addresses", func() {
-		FIt("should decode a valid zcash address", func() {
+		It("should decode a valid zcash address", func() {
 			address, err := DecodeAddress("tmXj1bXqHFU9toMhLnAwFad5JcehNNqGASy")
 			Expect(err).Should(BeNil())
 			Expect(address.String()).Should(Equal("tmXj1bXqHFU9toMhLnAwFad5JcehNNqGASy"))
